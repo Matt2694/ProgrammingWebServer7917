e7a6e6fb21fd5a47d2ffa1c9187fabb4fb3ff953
Matthew Peterson <matt2694@edu.eal.dk>
2017-09-07 09:58:47 +0200

    Add project files.

---
+++ ProgrammingWebServer7917/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammingWebServer7917
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.Run();
+        }
+
+        private void Run()
+        {
+            Server server = new Server();
+        }
+    }
+}
---
+++ ProgrammingWebServer7917/Server.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammingWebServer7917
+{
+    public class Server
+    {
+        #region Exercise 1
+
+        private TcpListener listener = null;
+        private bool running = true;
+
+        public Server()
+        {
+            listener = new TcpListener(IPAddress.Any, 80);
+            listener.Start();
+            TcpClient client = listener.AcceptTcpClient();
+            StreamReader sr = new StreamReader(client.GetStream());
+            StreamWriter sw = new StreamWriter(client.GetStream());
+            string output = "";
+            DateTime dateNow;
+            string returnRequest;
+            if (client.Connected)
+            {
+                while (running)
+                {
+                    string request = sr.ReadLine();
+                    dateNow = DateTime.Now;
+                    switch (request)
+                    {
+                        case "GET /date HTTP/1.1":
+                            returnRequest = dateNow.ToString("dd/MM/yyyy", CultureInfo.InvariantCulture);
+                            output = "HTTP/1.1 200 OK\nContent-Type: text/plain\nContent-Length: " + returnRequest.Length + "\n\n" + returnRequest;
+                            sw.WriteLine(output);
+                            sw.Flush();
+                            break;
+                        case "GET /time HTTP/1.1":
+                            returnRequest = dateNow.ToString("HH:mm:ss", CultureInfo.InvariantCulture);
+                            output = "HTTP/1.1 200 OK\nContent-Type: text/plain\nContent-Length: " + returnRequest.Length + "\n\n" + returnRequest;
+                            sw.WriteLine(output);
+                            sw.Flush();
+                            break;
+                        default:
+                            sw.WriteLine("404 Page Not Found.");
+                            running = false;
+                            break;
+                    }
+                }
+            }
+        }
+
+        #endregion
+    }
+}
